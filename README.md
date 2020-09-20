e97c9f92d2b4a2fa56727f49f8c5245d76e10165
StudioEtrange <nomorgan@gmail.com>
2020-09-20 23:27:27 +0200

    init

---
+++ README.md
@@ -0,0 +1,120 @@
+
+
+# [studioetrange/calibre-mod](https://github.com/studioetrange/docker-calibre-mod)
+
+
+calibre-mod **is not a standalone container** but an additional docker layer for linuxserver.io's containers which adds the binaries and dependencies necessary to enable ebook conversion on x86-64 utilising Calibre.
+
+## About this fork
+
+* This is a fork of a calibre-web mod [linuxserver/calibre-web:calibre](https://github.com/linuxserver/docker-calibre-web/tree/calibre)
+* Create an empty calibre database, if you don't have any at startup
+* Set some default settings for calibre binary
+* For each calibre version match a calibre-mod version
+* some ideas taken from [technosoft2000/calibre-web](https://github.com/Technosoft2000/docker-calibre-web) and from [thraxis/lazylibrarian-calibre](https://github.com/Thraxis/docker-lazylibrarian-calibre)
+
+## About linuxserver.io docker-mods
+
+* see https://github.com/linuxserver/docker-mods
+* see https://github.com/linuxserver/docker-baseimage-ubuntu/blob/bionic/root/docker-mods
+
+
+
+## Supported Architectures
+
+Our images support multiple architectures such as `x86-64`, `arm64` and `armhf`. We utilise the docker manifest for multi-platform awareness. More information is available from docker [here](https://github.com/docker/distribution/blob/master/docs/spec/manifest-v2-2.md#manifest-list) and our announcement [here](https://blog.linuxserver.io/2019/02/21/the-lsio-pipeline-project/). 
+
+Simply pulling `linuxserver/calibre-web` should retrieve the correct image for your arch, but you can also pull specific arch images via tags.
+
+The architectures supported by this image are:
+
+| Architecture | Tag |
+| :----: | --- |
+| x86-64 | amd64-latest |
+
+
+## Usage
+
+Here are some example snippets to help you get started creating a Calibre-Web container utilising this mod.
+
+
+
+### docker
+
+```
+docker create \
+  --name=calibre-web \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Europe/London \
+  -e DOCKER_MODS=studioetrange/calibre-mod:latest \
+  -p 8083:8083 \
+  -v <path to data>:/config \
+  -v <path to calibre library>:/books \
+  --restart unless-stopped \
+  linuxserver/calibre-web
+```
+
+
+### docker-compose
+
+Compatible with docker-compose v2 schemas.
+
+```
+---
+version: "2"
+services:
+  calibre-web:
+    image: linuxserver/calibre-web
+    container_name: calibre-web
+    environment:
+      - PUID=1000
+      - PGID=1000
+      - TZ=Europe/London
+      - DOCKER_MODS=studioetrange/calibre-mod:latest
+    volumes:
+      - <path to data>:/config
+      - <path to calibre library>:/books
+    ports:
+      - 8083:8083
+    restart: unless-stopped
+```
+
+## Docker tags
+
+Pre-setted build tags for studioetrange/calibre-mod:__TAG__
+
+	 latest, v4.23.0, v4.22.0, v4.21.0, v4.20.0, v4.19.0, v4.18.0, v4.17.0, v4.16.0, v4.15.0, v4.14.0, v4.13.0, v4.12.0, v4.11.2, v4.11.1, v4.11.0, v4.10.1, v4.10.0, v4.9.1, v4.9.0, v4.8.0, v4.7.0, v4.6.0, v4.5.0, v4.4.0, v4.3.0, v4.2.0, v4.1.0, v4.0.0, v3.48.0, v3.47.1
+
+Current latest tag is version __v4.23.0__
+
+## Application Setup
+
+In the Calibre-Web admin page (Basic Configuration:External Binaries) set the path to converter tool to `/usr/bin/ebook-convert`
+
+## Building locally
+
+If you want to make local modifications to these images for development purposes or just to customize the logic: 
+```
+git clone https://github.com/studioetrange/docker-calibre-mod.git
+cd docker-calibre-mod
+docker build \
+  --no-cache \
+  --pull \
+  -t studioetrange/calibre-mod .
+```
+
+But to use it with calibre-web you have to upload it to docker hub first:
+```
+docker push studioetrange/calibre-mod:latest
+```
+
+
+
+## Notes on Github / Docker Hub Repository
+
+* This github repository is linked to an automated build in docker hub registry.
+
+	https://registry.hub.docker.com/u/studioetrange/docker-calibre-mod/
+
+* _update.sh_ is only an admin script for this project which update and add new versions. This script do not auto create missing tag in docker hub webui. It is only for this project admin/owner purpose.
---
+++ update.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+_CURRENT_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+_CURRENT_RUNNING_DIR="$( cd "$( dirname "." )" && pwd )"
+
+STELLA_LOG_STATE="OFF"
+. "${_CURRENT_FILE_DIR}/stella-link.sh" include
+
+cd "${_CURRENT_FILE_DIR}"
+
+# PARAMETERS ----------------------------
+GITHUB_REPO="kovidgoyal/calibre"
+# remove this string to tag/release name  to use it as docker version (ex : with 'build' value, 'build507' becomes '507')
+DOCKER_TAG_FILTER_NAME=""
+
+VERSION_SEPARATOR="."
+# revert version ordering for letter behind number (ex: with 'ON' value, 1.2 is newer than 1.2rc1)
+VERSION_INVERSE_LAST_CHAR="OFF"
+
+# Use github tags instead of github releases
+USE_TAG_AS_RELEASE="0"
+
+# exclude some unwanted/malformed tag/release name
+EXCLUDE_VERSION="0.9.37 0.9.36 0.9.35 0.9.34 0.9.33"
+
+# see filter_version_list doc in https://github.com/StudioEtrange/stella/blob/stable/nix/common/common.sh
+VERSION_CONSTRAINT=">=v3.47.1"
+
+# GITHUB AUTHENTIFICATION --------------------------------
+# use your account to request github api
+#	./update.sh username:password
+CURL_AUTH=
+GITHUB_BASIC_AUTH="$1"
+if [ ! -z "${GITHUB_BASIC_AUTH}" ]; then
+	CURL_AUTH="-u ${GITHUB_BASIC_AUTH}"
+fi
+
+# INITIALIZE ----------------------------
+version_order_option=
+if [ "${VERSION_INVERSE_LAST_CHAR}" = "ON" ]; then
+	version_order_option="ENDING_CHAR_REVERSE"
+	[ ! "${VERSION_SEPARATOR}" = "" ] && version_order_option="${version_order_option} SEP ${VERSION_SEPARATOR}"
+fi
+
+# UPDATE FUNCTIONS ----------------------------
+update_dockerfile() {
+	local path="$1"
+	local version="$2"
+
+	sed -i.bak -e "s,ENV SERVICE_VERSION.*,ENV SERVICE_VERSION ${version}," "${path}"
+	rm -f "${path}.bak"
+}
+
+
+update_readme() {
+	local path="$1"
+	local version="$2"
+	local list="$3"
+
+	sed -i.bak -e "s/latest,.*/latest, ${list}/" "${path}"
+	sed -i.bak -e "s/^Current latest tag is version __.*__/Current latest tag is version __"${version}"__/" "${path}"
+	
+	rm -f "${path}.bak"
+}
+
+
+# GENERIC FUNCTIONS ----------------------------
+
+create_version_folder() {
+	local docker_tag_version="$1"
+	local service_version="$2"
+
+	echo " * Create release ${service_version}"
+	echo " ** Docker tag : ${docker_tag_version}"
+
+	mkdir -p "${_CURRENT_FILE_DIR}/ver/${docker_tag_version}"
+	cp -f "${_CURRENT_FILE_DIR}/Dockerfile" "ver/${docker_tag_version}/Dockerfile"
+	update_dockerfile "${_CURRENT_FILE_DIR}/ver/${docker_tag_version}/Dockerfile" "${service_version}"
+	mkdir -p "${_CURRENT_FILE_DIR}/ver/${docker_tag_version}/files"
+	cp -R "${_CURRENT_FILE_DIR}"/files/* "${_CURRENT_FILE_DIR}/ver/${docker_tag_version}/files/"
+
+}
+
+# GITHUB FUNCTIONS ----------------------------
+
+
+# github releases have a 'name' which is a version name and a 'tag_name' wich is often a version number
+# so we use 'tag_name' attribute
+github_releases() {
+	local nb="$1"
+
+	local result=""
+	local last_page=$(curl ${CURL_AUTH} -i -sL "https://api.github.com/repos/${GITHUB_REPO}/releases" | grep rel=\"last\" | cut -d "," -f 2 | cut -d "=" -f 2 | cut -d ">" -f 1)
+	for i in $(seq 1 ${last_page}); do 
+		result="${result} $(curl ${CURL_AUTH} -sL https://api.github.com/repos/${GITHUB_REPO}/releases?page=${i} | grep tag_name | cut -d '"' -f 4)"
+	done
+
+	result="$($STELLA_API filter_list_with_list "${result}" "${EXCLUDE_VERSION}" "FILTER_REMOVE")"
+
+	local sorted
+
+	if [ "${VERSION_CONSTRAINT}" = "" ]; then
+		[ "${nb}" = "" ] && sorted="$($STELLA_API sort_version "${result}" "DESC ${version_order_option}")" \
+			|| sorted="$($STELLA_API sort_version "${result}" "DESC ${version_order_option} LIMIT ${nb}")"
+	else
+		[ "${nb}" = "" ] && sorted="$($STELLA_API filter_version_list "${VERSION_CONSTRAINT}" "${result}" "DESC ${version_order_option}")" \
+			|| sorted="$($STELLA_API filter_version_list "${VERSION_CONSTRAINT}" "${result}" "DESC ${version_order_option} LIMIT ${nb}")"
+	fi
+
+	echo "${sorted}"
+}
+
+# github tags have only a 'name' which is often a version number
+# so we use 'name' attribute
+github_tags() {
+	local nb="$1"
+
+	local result=""
+	local last_page=$(curl ${CURL_AUTH} -i -sL "https://api.github.com/repos/${GITHUB_REPO}/tags" | grep rel=\"last\" | cut -d "," -f 2 | cut -d "=" -f 2 | cut -d ">" -f 1)
+	for i in $(seq 1 ${last_page}); do 
+		result="${result} $(curl ${CURL_AUTH} -sL https://api.github.com/repos/${GITHUB_REPO}/tags?page=${i} | grep name | cut -d '"' -f 4)"
+	done
+
+	result="$($STELLA_API filter_list_with_list "${result}" "${EXCLUDE_VERSION}" "FILTER_REMOVE")"
+
+	local sorted
+
+	if [ "${VERSION_CONSTRAINT}" = "" ]; then
+		[ "${nb}" = "" ] && sorted="$($STELLA_API sort_version "${result}" "DESC ${version_order_option}")" \
+			|| sorted="$($STELLA_API sort_version "${result}" "DESC ${version_order_option} LIMIT ${nb}")"
+	else
+		[ "${nb}" = "" ] && sorted="$($STELLA_API filter_version_list "${VERSION_CONSTRAINT}" "${result}" "DESC ${version_order_option}")" \
+			|| sorted="$($STELLA_API filter_version_list "${VERSION_CONSTRAINT}" "${result}" "DESC ${version_order_option} LIMIT ${nb}")"
+	fi
+	
+	echo "${sorted}"
+}
+
+
+
+
+
+
+
+
+
+
+# MAIN ----------------------------
+rm -Rf "ver"
+
+# sed regexp can not be empty, so a foo string is used
+[ "${DOCKER_TAG_FILTER_NAME}" = "" ] && DOCKER_TAG_FILTER_NAME="XXXXXXXXXXXX"
+
+
+echo
+echo "******** UPDATE LAST ********"
+# Update last release
+
+if [ "${USE_TAG_AS_RELEASE}" = "1" ]; then
+	last_release=$(github_tags "1")
+else
+	last_release=$(github_releases "1")
+fi
+
+echo " * Create release ${last_release}"
+echo " ** Docker tag : latest"
+update_dockerfile "${_CURRENT_FILE_DIR}/Dockerfile" "${last_release}"
+
+
+echo
+echo "******** UPDATE ALL ********"
+# Update all releases
+if [ "${USE_TAG_AS_RELEASE}" = "1" ]; then
+	releases="$(github_tags)"
+else
+	releases="$(github_releases)"
+fi
+
+for rel in ${releases}; do
+
+	docker_version_number="$(echo ${rel} | sed -e "s,${DOCKER_TAG_FILTER_NAME},,g")"
+	create_version_folder "${docker_version_number}" "${rel}" 
+
+	echo
+done
+
+echo "******** UPDATE README ********"
+list_release=$(echo ${releases} | sed -e "s,${DOCKER_TAG_FILTER_NAME},,g" | sed -e 's/ /\, /g')
+last_release_tag=$(echo ${last_release} | sed -e "s,${DOCKER_TAG_FILTER_NAME},,g")
+
+update_readme "README.md" "${last_release_tag}" "${list_release}"
+
+echo
+echo "************************************"
+echo " YOU SHOULD NOW ADD MISSING VERSION THROUGH"
+echo " Docker Hub WebUI : AUTOMATED BUILD REPOSITORY"
